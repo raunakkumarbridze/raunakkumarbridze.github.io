57d013b9012954b4107405a6110379b78e8140dc
Raunak Kumar <raunak.kumar@bridzeresearch.com>
2020-06-02 23:30:43 +0530

    First commit_2

--- js/slidebar.js
+++ js/slidebar.js
@@ -1,18 +1,3 @@
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-
 //---------------------Slideshow---------------------------------
 
 var slideIndex = [1,1];
